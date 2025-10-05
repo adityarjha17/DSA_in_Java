839deead1c3bd7995830a2617a2023606dc302a4
adityarjha17 <jha.aditya17@gmail.com>
2025-10-05 11:31:29 +0530

    Linear Search and Union(sorted and unsorted) added

---
+++ arrayOps/arrayQs/unionOf2SortedArrays.java
@@ -0,0 +1,43 @@
+package arrayOps.arrayQs;
+import arrayOps.Sorting.*;
+import java.util.*;
+public class unionOf2SortedArrays{
+    public static void checkIfPresent(ArrayList<Integer> list, int val){
+        if(list.isEmpty() || list.get(list.size()-1) != val){
+            list.add(val);
+        }
+    }
+    public static ArrayList<Integer> findUnion(int a[], int b[]) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int i=0, j=0;
+        while(i<a.length & j<b.length){
+            if(a[i]<b[i]){
+                checkIfPresent(list, a[i]);
+                i++;
+            } else if(a[i]>b[j]){
+                checkIfPresent(list, b[j]);
+                j++;
+            } else {
+                checkIfPresent(list, a[i]);
+                i++;
+                j++;
+            }
+        }
+        while(i<a.length){
+            checkIfPresent(list, a[i]);
+            i++;
+        }
+        while(j<b.length){
+            checkIfPresent(list, b[j]);
+            j++;
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        int arr[] = infoArray.inputArray(5);
+        infoArray.printValues(arr);
+        int arr2[]= infoArray.inputArray(5);
+        infoArray.printValues(arr2);
+        System.out.println(findUnion(arr, arr2));
+    }
+}
---
+++ arrayOps/arrayQs/unionOf2UnsortedArrays.java
@@ -0,0 +1,24 @@
+package arrayOps.arrayQs;
+import java.util.*;
+
+import arrayOps.Sorting.infoArray;
+public class unionOf2UnsortedArrays {
+    public static ArrayList<Integer> findUnion(int a[], int b[]){
+        Set<Integer> set = new TreeSet<>();
+        for(int i: a){
+            set.add(i);
+        }
+        for(int j: b){
+            set.add(j);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>(set);
+        //return new ArrayList<>(set);
+        return list;
+    }
+    public static void main(String[] args) {
+        int a[] = infoArray.inputArray(5);
+        int b[] = infoArray.inputArray(5);
+        System.out.println(findUnion(a, b));
+    }
+}
